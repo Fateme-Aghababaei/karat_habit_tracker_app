eca6a817d5848559f7a464528d9cdbea65096e6a
Motahare <mv2971@yahoo.com>
2024-07-27 01:17:59 +0330

    add navbar component

---
+++ lib/view/challenge_screen/challenge_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:karat_habit_tracker_app/utils/routes/RouteNames.dart';
+import 'package:karat_habit_tracker_app/view/signup_screen/signup_controller.dart';
+
+import '../components/BottomNavigationBar.dart';
+
+class HabitPage extends StatefulWidget {
+  HabitPage({super.key});
+
+  @override
+  State<HabitPage> createState() => _HabitPageState();
+}
+
+class _HabitPageState extends State<HabitPage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        bottomNavigationBar: CustomBottomNavigationBar()
+    );
+  }
+}
